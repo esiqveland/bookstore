06fe4e430da5c0819ee97f77285a0484f689ffe7
Eivind Siqveland Larsen <to.eivind@gmail.com>
2013-10-07 09:42:43 +0200

    Only allows sending customer support emails to allowed emails.

--- src/java/amu/action/CustomerSupportAction.java
+++ src/java/amu/action/CustomerSupportAction.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpSession;
 
 class CustomerSupportAction implements Action {
 
+    public static String[] allowed_departments = {"tdt4237.amu.darya@gmail.com", "tdt4237.amu.darya@gmail.com"};
+
     @Override
     public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         HttpSession session = request.getSession(true);
@@ -20,15 +22,25 @@ class CustomerSupportAction implements Action {
         }
 
         if (request.getMethod().equals("POST")) {
-            Mailer.send(request.getParameter("department"), 
-                    request.getParameter("subject"), 
-                    request.getParameter("content"), 
-                    request.getParameter("fromAddr"), 
-                    request.getParameter("fromName"));
-            // TODO: Send receipt to customer
-            return new ActionResponse(ActionResponseType.REDIRECT, "customerSupportSuccessful");
+            if(isValidDepartment(request.getParameter("department"))) {
+                Mailer.send(request.getParameter("department"),
+                        request.getParameter("subject"),
+                        request.getParameter("content"),
+                        request.getParameter("fromAddr"),
+                        request.getParameter("fromName"));
+                // TODO: Send receipt to customer
+                return new ActionResponse(ActionResponseType.REDIRECT, "customerSupportSuccessful");
+            }
         } 
 
         return new ActionResponse(ActionResponseType.FORWARD, "customerSupport");
+    }
+
+    private boolean isValidDepartment(String department) {
+        for(String valid : allowed_departments) {
+            if(department.equals(valid))
+                return true;
+        }
+        return false;
     }
 }

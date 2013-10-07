557bec390c2434fab7a49db2f1b3bf6fd058984f
Eivind Siqveland Larsen <to.eivind@gmail.com>
2013-10-07 10:01:14 +0200

    BookDAO: use preparedstatements

--- src/java/amu/database/BookDAO.java
+++ src/java/amu/database/BookDAO.java
@@ -10,21 +10,19 @@ import java.util.logging.*;
 public class BookDAO {
     public Book findByISBN(String isbn) {
         Book book = null;
-        
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement statement = null;
         ResultSet resultSet = null;
         
         try {
             connection = Database.getConnection();
-            statement = connection.createStatement();
-            
-            String query = "SELECT * FROM book, publisher, title "
-                    + "WHERE book.isbn13 = '"
-                    + isbn + "' "
-                    + "AND book.title_id = title.id "
-                    + "AND book.publisher_id = publisher.id;";
-            resultSet = statement.executeQuery(query);
+
+            String query = "SELECT * FROM book, publisher, title, WHERE book.isbn13=? " +
+                    "AND book.title_id = title.id AND book.publisher_id = publisher.id";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, isbn);
+
+            resultSet = statement.executeQuery();
             Logger.getLogger(this.getClass().getName()).log(Level.FINE, "findByISBN SQL Query: " + query);
             
             if (resultSet.next()) {
